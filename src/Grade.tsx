761a0ed09afd572cf6f4f85679eea69c26338bca
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-11-22 18:53:32 -0300

    Add codigo e nome das disciplinas

--- src/Grade.tsx
+++ src/Grade.tsx
@@ -1,14 +1,47 @@
-import Calculo from  "./Calculo1"
+import Calculo from "./Calculo1";
 
 export default function Grade() {
   return (
     <>
-    <div className='grade'>
-            <Calculo codigo='Cálculo 1' nome='Cálculo 1' horario= {1230}/>
-            <Calculo codigo="xxxxxxxx" nome= "Geometria analitica" horario= {1230}/>
-            <Calculo codigo ="xxxxxx" nome ="algo" horario= {1230}/>
-            
-            </div> 
+      <div className="grade">
+        <Calculo
+          codigo="DCC160 B"
+          nome="LÓGICA FUNDAMENTOS PARA A COMPUTAÇÃO "
+          horario={1230}
+        />
+        <Calculo codigo="DCC200 A" nome="ALGORITMOS II" horario={1230} />
+        <Calculo
+          codigo="DCC207 A"
+          nome="DESENVOLVIMENTO WEB FRONT-END"
+          horario={1230}
+        />
+        <Calculo
+          codigo="DC5200 A"
+          nome="ALGORITMOS II PRÁTICA  "
+          horario={1230}
+        />
+        <Calculo
+          codigo="EADDCC044 A"
+          nome="INFORMÁTICA E SOCIEDADE"
+          horario={1230}
+        />
+        <Calculo
+          codigo="FIN001 A"
+          nome="CONTABILIDADE GERAL E INTRODUTORIA"
+          horario={1230}
+        />
+        <Calculo codigo="MAT154E Y" nome="CÁLCULO I" horario={1230} />
+        <Calculo
+          codigo="MAT155 A"
+          nome="GEOMETRIA ANALÍTICA E SISTEMAS"
+          horario={1230}
+        />
+        <Calculo
+          codigo="UNI001 G"
+          nome="LINGUA INGLESA INSTRUMENTAL I"
+          horario={1230}
+        />
+      </div>
     </>
-  )
+  );
 }
